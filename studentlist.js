1e27473444518a3d05d99300b96deb49f103b3f6
kauniete <38038319+kauniete@users.noreply.github.com>
2019-02-27 23:31:10 +0100

    started over again

--- studentlist.js
+++ studentlist.js
@@ -1,37 +1,50 @@
 "use strict";
-
+const baseLink = "https://petlatkea.dk/2019/hogwarts/students.json";
+const studentObject = {
+  firstname: "-student first name-",
+  lastname: "-student last name-",
+  house: "-student house-"
+};
+let arrayOfStudents = [];
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
   console.log("init");
-
   // TODO: Load JSON, create clones, build list, add event listeners, show modal, find images, and other stuff ...
   getJSON();
 }
-
 function getJSON() {
+  fetch(baseLink)
+    .then(event => event.json())
+    .then(data => createObject(data));
   console.log("getJSON");
-
-  // NOTE: Maybe also call sortByFirst the first time ... Investigate!
-  filterList();
 }
 
-function filterList() {
-  console.log("filterList");
-
-  displayList();
+function createObject(data) {
+  data.forEach(element => {
+    const astudent = Object.create(studentObject);
+    //console.log(astudent);
+    let name = element.fullname;
+    astudent.firstname = name.substring(0, name.indexOf(" "));
+    astudent.lastname = name.substring(name.lastIndexOf(" ") + 1);
+    astudent.house = element.house;
+    arrayOfStudents.push(astudent);
+  });
+  console.log(arrayOfStudents);
+  displayList(arrayOfStudents);
 }
+function displayList(arrayOfStudents) {
+  //console.log(arrayOfStudents);
+  document.querySelector("#list").innerHTML = "";
+  arrayOfStudents.forEach(student => {
+    //console.log(student.firstname);
+    const template = document.querySelector("#studentTemplate").content;
+    const clone = template.cloneNode(true);
+    clone.querySelector(".first span").textContent = student.firstname;
+    clone.querySelector(".last span").textContent = student.lastname;
+    clone.querySelector(".house span").textContent = student.house;
 
-function displayList() {
-  console.log("displayList");
-}
-
-// TODO: Create scaffolding functions for the rest!
-
-function clickSortByFirst() {
-
-}
-
-function sortListByFirst() {
-
+    clone.querySelector("li").id = student.firstname;
+    document.querySelector("#list").appendChild(clone);
+  });
 }
